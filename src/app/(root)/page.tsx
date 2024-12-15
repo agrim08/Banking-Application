df781848c125d020deff0b83ad8e8ff0adc92d06
agrim08 <agrimgupta0805@gmail.com>
2024-12-15 21:52:12 +0530

    home page basic ui completed and sidebar routing setup

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Agrim" };
+  const loggedIn = {
+    firstName: "Agrim",
+    lastName: "Gupta",
+    email: "gupta@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -14,13 +19,14 @@ const Home = () => {
             user={loggedIn?.firstName || "Guest"}
             subText="Manage your banking transactions efficiently"
           />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1760.0}
+          />
         </header>
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1760.0}
-        />
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
